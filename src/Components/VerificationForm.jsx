76fee5b95838e3e8090212e4c6e06accc13d6256
Omkar Patil <Omkarp5429@gmail.com>
2023-11-12 22:29:09 +0530

    Feat: Resend OTP feature

--- src/Components/VerificationForm.jsx
+++ src/Components/VerificationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Box, Typography, Button, TextField } from "@mui/material";
 import styled from "@emotion/styled";
@@ -21,6 +21,7 @@ const VerificationForm = () => {
   });
 
   const [isClicked, setIsClicked] = useState(false);
+  const [timer, setTimer] = useState(90);
 
   const StyledTypography = styled(Typography)({
     color: "white",
@@ -38,7 +39,7 @@ const VerificationForm = () => {
   });
 
   const StyledTextField = styled(TextField)({
-    width: '2.5rem',
+    width: "2.5rem",
     overflow: "hidden",
   });
 
@@ -107,6 +108,46 @@ const VerificationForm = () => {
     }
   };
 
+  const handleSend = async () => {
+    try {
+      const response = await fetch(
+        "https://otp-service-beta.vercel.app/api/otp/generate",
+        {
+          method: "POST",
+          body: JSON.stringify(location.state),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+      if (result.message) {
+        dispatch({ type: "SET_LOADING", payload: false });
+        toast.success(result.message);
+        navigate("/verify", {
+          state: location.state,
+        });
+      } else {
+        dispatch({ type: "SET_LOADING", payload: false });
+        toast.error(`${result.error}, Please try after some time!`);
+      }
+    } catch (error) {
+      dispatch({ type: "SET_LOADING", payload: false });
+      toast.error(`${error}, Please try after some time!`);
+    }
+  };
+
+  useEffect(() => {
+    if (timer <= 0) return;
+
+    const timerInterval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timerInterval);
+  }, [timer]);
+
   return (
     <StyledBox>
       <AssignmentInd
@@ -124,107 +165,158 @@ const VerificationForm = () => {
       <Typography sx={{ color: "white" }}>
         Enter the OTP below to verify it
       </Typography>{" "}
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          marginTop: "30px",
-          justifyContent: "space-between",
-        }}
-      >
-        <StyledTextField
-          id="otpOne"
-          name="otpOne"
-          autoFocus={!checkCondition(otpInput)}
-          value={otpInput.otpOne}
-          required
-          placeholder="*"
-          onChange={handleChange}
-          inputProps={{
-            maxLength: 1,
-            style: inputTextStyle,
-          }}
-        />
-        <StyledTextField
-          id="otpTwo"
-          name="otpTwo"
-          autoFocus={
-            otpInput.otpOne.length && !otpInput.otpThree.length ? true : false
-          }
-          value={otpInput.otpTwo}
-          required
-          placeholder="*"
-          onChange={handleChange}
-          inputProps={{
-            maxLength: 1,
-            style: inputTextStyle,
-          }}
-        />
-        <StyledTextField
-          id="otpThree"
-          name="otpThree"
-          autoFocus={
-            otpInput.otpTwo.length && !otpInput.otpFour.length ? true : false
-          }
-          value={otpInput.otpThree}
-          required
-          placeholder="*"
-          onChange={handleChange}
-          inputProps={{
-            maxLength: 1,
-            style: inputTextStyle,
-          }}
-        />
-        <StyledTextField
-          id="otpFour"
-          name="otpFour"
-          autoFocus={
-            otpInput.otpThree.length && !otpInput.otpFour.length ? true : false
-          }
-          value={otpInput.otpFour}
-          required
-          placeholder="*"
-          onChange={handleChange}
-          inputProps={{
-            maxLength: 1,
-            style: inputTextStyle,
-          }}
-        />
-        <StyledTextField
-          id="otpFive"
-          name="otpFive"
-          autoFocus={
-            otpInput.otpFour.length && !otpInput.otpSix.length ? true : false
-          }
-          value={otpInput.otpFive}
-          required
-          placeholder="*"
-          onChange={handleChange}
-          inputProps={{
-            maxLength: 1,
-            style: inputTextStyle,
-          }}
-        />
-        <StyledTextField
-          id="otpSix"
-          name="otpSix"
-          value={otpInput.otpSix}
-          autoFocus={otpInput.otpFive.length ? true : false}
-          required
-          placeholder="*"
-          onChange={handleChange}
-          inputProps={{
-            maxLength: 1,
-            style: inputTextStyle,
-          }}
-        />
-      </Box>
       {checkCondition(otpInput) === false && isClicked ? (
         <Typography sx={{ color: "red", marginTop: "10px", fontSize: "12px" }}>
           Please Fill all the fields*
         </Typography>
       ) : (
         ""
+      )}
+      <Box position="relative">
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            marginTop: "10px",
+            justifyContent: "space-between",
+          }}
+        >
+          <StyledTextField
+            id="otpOne"
+            name="otpOne"
+            autoFocus={!checkCondition(otpInput)}
+            value={otpInput.otpOne}
+            required
+            placeholder="*"
+            onChange={handleChange}
+            inputProps={{
+              maxLength: 1,
+              style: inputTextStyle,
+            }}
+          />
+          <StyledTextField
+            id="otpTwo"
+            name="otpTwo"
+            autoFocus={
+              otpInput.otpOne.length && !otpInput.otpThree.length ? true : false
+            }
+            value={otpInput.otpTwo}
+            required
+            placeholder="*"
+            onChange={handleChange}
+            inputProps={{
+              maxLength: 1,
+              style: inputTextStyle,
+            }}
+          />
+          <StyledTextField
+            id="otpThree"
+            name="otpThree"
+            autoFocus={
+              otpInput.otpTwo.length && !otpInput.otpFour.length ? true : false
+            }
+            value={otpInput.otpThree}
+            required
+            placeholder="*"
+            onChange={handleChange}
+            inputProps={{
+              maxLength: 1,
+              style: inputTextStyle,
+            }}
+          />
+          <StyledTextField
+            id="otpFour"
+            name="otpFour"
+            autoFocus={
+              otpInput.otpThree.length && !otpInput.otpFour.length
+                ? true
+                : false
+            }
+            value={otpInput.otpFour}
+            required
+            placeholder="*"
+            onChange={handleChange}
+            inputProps={{
+              maxLength: 1,
+              style: inputTextStyle,
+            }}
+          />
+          <StyledTextField
+            id="otpFive"
+            name="otpFive"
+            autoFocus={
+              otpInput.otpFour.length && !otpInput.otpSix.length ? true : false
+            }
+            value={otpInput.otpFive}
+            required
+            placeholder="*"
+            onChange={handleChange}
+            inputProps={{
+              maxLength: 1,
+              style: inputTextStyle,
+            }}
+          />
+          <StyledTextField
+            id="otpSix"
+            name="otpSix"
+            value={otpInput.otpSix}
+            autoFocus={
+              otpInput.otpFive.length && checkCondition(otpInput) === false
+                ? true
+                : false
+            }
+            required
+            placeholder="*"
+            onChange={handleChange}
+            inputProps={{
+              maxLength: 1,
+              style: inputTextStyle,
+            }}
+          />
+        </Box>
+        <Button
+          sx={{
+            position: "absolute",
+            right: 0,
+            bottom: "-100%",
+            color: "#ff1744",
+          }}
+          onClick={() => {
+            setIsClicked(false);
+            setOtpInput({
+              otpOne: "",
+              otpTwo: "",
+              otpThree: "",
+              otpFour: "",
+              otpFive: "",
+              otpSix: "",
+            });
+          }}
+        >
+          Clear
+        </Button>
+      </Box>
+      {timer > 0 ? (
+        <Typography
+          sx={{ color: "#76ff03", marginTop: "10px", fontSize: "12px" }}
+        >
+          Didn't get OTP? Resend in {timer} seconds!
+        </Typography>
+      ) : (
+        <Typography
+          sx={{
+            color: "#ffea00",
+            marginTop: "10px",
+            fontSize: "12px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            dispatch({ type: "SET_LOADING", payload: true });
+            handleSend();
+          }}
+        >
+          Resend OTP
+        </Typography>
       )}
       <Button
         variant="contained"
